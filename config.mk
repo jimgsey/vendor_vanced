ce559734e5a0607b67e2f6dfd961114a2b21ff10
TeraaBytee <terabyte3766@gmail.com>
2022-02-02 09:25:15 +0700

    vanced: Move to priv-app
    Signed-off-by: TeraaBytee <terabyte3766@gmail.com>

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 VENDOR_PATH := vendor/vanced
 
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/app/MicroG/lib/arm64,system/app/MicroG/lib/arm64) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/app/MicroG/oat/arm64,system/app/MicroG/oat/arm64) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/app/YouTubeVanced/lib/arm64,system/app/YouTubeVanced/lib/arm64) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/app/YouTubeVanced/oat/arm64,system/app/YouTubeVanced/oat/arm64)
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MicroG/lib/arm64,system/priv-app/MicroG/lib/arm64) \
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MicroG/oat/arm64,system/priv-app/MicroG/oat/arm64) \
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/YouTubeVanced/lib/arm64,system/priv-app/YouTubeVanced/lib/arm64) \
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/YouTubeVanced/oat/arm64,system/priv-app/YouTubeVanced/oat/arm64)
 
 PRODUCT_PACKAGES += \
 	Manager \
