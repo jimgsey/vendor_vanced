f8e80789398c6a8c55239ee74961db677b7c8a65
Jimgsey <jimgsei@gmail.com>
2022-03-04 20:07:04 +0100

    vanced: I dont use account

--- config.mk
+++ config.mk
@@ -1,12 +1,8 @@
 VENDOR_PATH := vendor/vanced
 
 PRODUCT_COPY_FILES += \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MicroG/lib/arm64,system/priv-app/MicroG/lib/arm64) \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MicroG/oat/arm64,system/priv-app/MicroG/oat/arm64) \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/YouTubeVanced/lib/arm64,system/priv-app/YouTubeVanced/lib/arm64) \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/YouTubeVanced/oat/arm64,system/priv-app/YouTubeVanced/oat/arm64)
 
 PRODUCT_PACKAGES += \
-	Manager \
-	MicroG \
 	YouTubeVanced
